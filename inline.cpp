7869489b7220c4855e36a12d42ea2f868f3edbbd
rxsi_xx_lt <rxsi_xx_lt>
2021-12-05 22:30:19 +0800

    虚函数的内联

--- inline.cpp
+++ inline.cpp
@@ -12,9 +12,15 @@ F(i++) 会被展开会 (i++ + i++), 造成BUG
 是在编译期进行替换,同样节省了函数调用的入栈出栈开销
 会进行类型的检查
 但是当有多处调用时,会在每个调用处都进行展开,导致函数体变大,内存开销变大
-类中的成员函数都是默认的内联函数
+在类中定义的成员函数都是默认的内联函数(除了虚函数,虚函数需要显式的添加inline),而只是声明函数,则需要把类外定义的函数加上inline修饰
 
 
+ * 编译器对 inline 函数的处理步骤
+ * 将 inline 函数体复制到 inline 函数调用点处；
+ * 为所用 inline 函数中的局部变量分配内存空间；
+ * 将 inline 函数的的输入参数和返回值映射到调用方法的局部变量空间中；
+ * 如果 inline 函数有多个返回点，将其转变为 inline 函数代码块末尾的分支（使用 GOTO）。
+ 
 */
 
 // inline要与函数定义放在一起,函数声明可以不加inline
@@ -59,3 +65,51 @@ jmp指令的汇编代码地址，而jmp指令后面跟的才是“被调用的�
 
 
 */
+
+/*
+虚函数可以是内联函数，内联是可以修饰虚函数的，但是当虚函数表现多态性的时候不能内联。
+内联是在编译器建议编译器内联，而虚函数的多态性在运行期，编译器无法知道运行期调用哪个代码，
+因此虚函数表现为多态性时（运行期）编译器不会使之内联。
+inline virtual 唯一可以内联的时候是：
+编译器知道所调用的对象是哪个类（如 Base::who()），
+这只有在编译器具有实际对象而不是对象的指针或引用时才会发生。
+*/
+
+#include <iostream>  
+using namespace std;
+class Base
+{
+public:
+    inline virtual void who() // 显式的指定为内联
+    {
+        cout << "I am Base\n";
+    }
+    virtual ~Base() {}
+};
+class Derived : public Base
+{
+public:
+    inline void who()  // 不写inline时隐式内联
+    {
+        cout << "I am Derived\n";
+    }
+};
+
+int main()
+{
+    // 此处的虚函数 who()，是通过类（Base）的具体对象（b）来调用的，编译期间就能确定了，
+    // 所以它可以是内联的，但最终是否内联取决于编译器。 
+    Base b;
+    b.who();
+
+    // 此处的虚函数是通过指针调用的，呈现多态性，需要在运行时期间才能确定，所以不能为内联。  
+    Base *ptr = new Derived();
+    ptr->who();
+
+    // 因为Base有虚析构函数（virtual ~Base() {}），所以 delete 时，会先调用派生类（Derived）析构函数，再调用基类（Base）析构函数，防止内存泄漏。
+    delete ptr;
+    ptr = nullptr;
+
+    system("pause");
+    return 0;
+} 
