f4bf3d4274e006559347a26e88d188defd30b965
rxsi_dy_vm <rxsi_dy_vm>
2022-01-21 17:13:35 +0800

    std::thread类内线程函数

--- thread_test.cpp
+++ thread_test.cpp
@@ -26,25 +26,75 @@
 // }
 
 
-#include <stdio.h>
+// #include <stdio.h>
+// #include <thread>
+// #include <iostream>
+
+// void threadproc1()
+// {
+//     std::cout << "C++ std::thread 1" << std::endl;
+// }
+
+// void threadproc2(int a, int b)
+// {
+//     std::cout << "C++ std::thread 2" << std::endl;
+// }
+
+// int main()
+// {
+//     std::thread t1(threadproc1);
+//     std::thread t2(threadproc2, 1, 2);
+//     t1.join();
+//     t2.join();
+//     return 0;
+// }
+
+
 #include <thread>
+#include <memory>
+#include <stdio.h>
 #include <iostream>
 
-void threadproc1()
+class Thread
 {
-    std::cout << "C++ std::thread 1" << std::endl;
-}
+public:
+    Thread(){}
+    ~Thread(){}
+    void Start()
+    {
+        m_stopped = false;
+        m_spThread.reset(new std::thread(&Thread::threadFunc, this, 1, 2)); // 注意,因为调用的是实例对象,因此要主动添加this指针作为参数
+    }
 
-void threadproc2(int a, int b)
-{
-    std::cout << "C++ std::thread 2" << std::endl;
-}
+    void Stop()
+    {
+        m_stopped = true;
+        if (m_spThread)
+        {
+            if (m_spThread->joinable())
+            {
+                m_spThread->join();
+            }
+        }
+    }
+
+private:
+
+    void threadFunc(int arg1, int arg2)
+    {
+        while (!m_stopped)
+        {
+            std::cout << "Thread function use instance method." << std::endl;
+        }
+    }
+
+    std::shared_ptr<std::thread> m_spThread;
+    bool m_stopped;
+};
+
 
 int main()
 {
-    std::thread t1(threadproc1);
-    std::thread t2(threadproc2, 1, 2);
-    t1.join();
-    t2.join();
-    return 0;
+    Thread myThread;
+    myThread.Start();
 }
