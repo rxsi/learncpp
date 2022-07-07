139a85d33db4eb16a2008ee2d0f09d4795cab10a
rxsi_dy_pc <rxsi_dy_pc>
2022-07-07 17:25:21 +0800

    a星算法

--- algorithm/astar.cpp
+++ algorithm/astar.cpp
@@ -3,224 +3,183 @@
 #include <vector>
 #include <queue>
 #include <math.h>
+#include <unordered_map>
+#include <iostream>
 
 using namespace std;
-
-#define M INT_MAX / 2
-
-/*
-节点信息
-*/
-class Node 
-{
-public:
-    Node(int, int);
-    void setG(int);
-    void setH(int);
-    void setParent(shared_ptr<Node>);
-    int getX() const;
-    int getY() const;
-    int getF() const;
-    int getG() const;
-    bool isClose() const;
-    int setWeight(int);
-    void setClose();
-    shared_ptr<Node> getParent();
-
-private:
-    int x;
-    int y;
-    shared_ptr<Node> parent; // 指向父对象
-    int g;
-    int h;
-    int weight;
-    bool close; // 当为1时表示已经计算处最小,不再计算
-};
-
-Node::Node(int x, int y): x(x), y(y){}
-void Node::setG(int g)
-{
-    g = g;
-}
-
-void Node::setH(int h)
-{
-    h = h;
-}
-
-void Node::setParent(shared_ptr<Node> p)
-{
-    parent = p;
-}
-
-inline int Node::getX() const
-{
-    return x;
-}
-
-inline int Node::getY() const
-{
-    return y;
-}
-
-inline int Node::getF() const
-{
-    return x + y;
-}
-
-inline int Node::getG() const
-{
-    return g;
-}
-
-inline bool Node::isClose() const
-{
-    return isClose;
-}
-
-inline int Node::setWeight(int weight)
-{
-    weight = weight;
-}
-
-inline void Node::setClose()
-{
-    close = true;
-}
-
-shared_ptr<Node> Node::getParent()
-{
-    return parent;
-}
-
 
 class aStar
 {
 public:
-    using NodePtr = shared_ptr<Node>;
-    vector<pair<int, int>> search(vector<vector<int>>&, int, int, int, int);
-
+    using Map = vector<vector<int>>;
+    vector<pair<int, int>> search(const Map&, int, int, int, int);
 private:
-    inline bool _isValid(vector<vector<int>>&, int, int) const;
-    void _searchNear(const NodePtr&, const NodePtr&) const;
-    void _searchOneNode(Node&) const;
-    int _calG(vector<vector<int>>& map, shared_ptr<Node>& node1, shared_ptr<Node>& node2) const;
-    int _calH(const NodePtr&, const NodePtr&) const;
-    bool _isEndNode(const NodePtr&, const NodePtr&) const;
+    bool _isValid(const Map&, int, int);
+    int _calH(int, int, int, int);
+    int _calG(const Map&, int, int, int);
+    static int D; // 直线运动的代价
+    static int DD; // 斜线运动的代价
+    static vector<pair<int, int>> path;
+    int _maxX; // 当前地图X的最大值
+    int _maxY; // 当前地图Y的最大值
 
-private:
-    int row;
-    int col;
-    static int D;
-    static int DD;
+    struct Node
+    {
+        int x; // x轴位置
+        int y; // y轴位置
+        int g; // g值
+        int h; // h值
+        Node* parent; // 父节点
+        bool close; // 是否已经在close中，true是，false否
+        Node(int x, int y): x(x), y(y){}
+    };
     struct cmp
     {
-        bool operator()(NodePtr& m, NodePtr& n)
+        bool operator()(const Node* node1, const Node* node2)
         {
-            return m->getF() < n->getF();
+            return node1->g + node1->h < node2->g + node2->h;
         }
     };
-    
-    // vector<shared_ptr>;
 };
 
 int aStar::D = 10;
 int aStar::DD = 14;
+vector<pair<int, int>> path{
+    /*
+    搜索节点周围的点
+    按照八个方位搜索
+    (x-1,y-1)(x-1,y)(x-1,y+1)
+    (x  ,y-1)(x  ,y)(x  ,y+1)
+    (x+1,y-1)(x+1,y)(x+1,y+1)
+    */
+    {-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1} // 前四个方向是直线，后四个是斜线
+};
 
-// int aStar::_calG(shared_ptr<Node>& node1, shared_ptr<Node>& node2) const
-// {
-//     if (abs(node1->getX() - node2->getX() == 1 && abs(node1->getY() - node2->getY()) == 1)
-//     {
-//         return sqrt(2) * Node.Weight
-//     }
-
-// }
-
-int aStar::_calH(const NodePtr& node1, const NodePtr& node2) const
+bool aStar::_isValid(const Map& map, int x, int y)
 {
-    int dx = abs(node1->getX() - node2->getX());
-    int dy = abs(node2->getY() - node2->getY());
-    return (dx + dy) + (DD - 2*D) * min(dx, dy); // 对角距离
+    return 0 <= x < _maxX && 0 <= y < _maxY && map[x][y] > -1;
 }
 
-inline bool aStar::_isEndNode(const NodePtr& node1, const NodePtr& node2) const
+int aStar::_calH(int current_x, int current_y, int end_x, int end_y)
 {
-    return node1->getX() == node2->getX() && node1->getY() == node2->getY();
+    int dx = abs(current_x - end_x);
+    int dy = abs(current_y - end_y);
+    /*
+    三种计算方式：
+    1. 曼哈顿距离，适合只能直线运动的场景：(dx + dy) * D
+    2. 对角距离，适合直线+45度角运动的场景：(dx + dy) + (DD - 2*D) * min(dx, dy)
+    3. 欧几里得距离：适合任意角度运行，有sqrt操作，相对较慢，开方可以使用别的方式优化：D * sqrt(dx * dx + dy * dy)
+    */
+    return D * sqrt(dx * dx + dy * dy);
 }
 
-inline bool aStar::_isValid(vector<vector<int>>& map, int x, int y) const
+int aStar::_calG(const Map& map, int dest_x, int dest_y, int weight)
 {
-    return 0 <= x && x < this->row && 0 <= y && y < this->col && map[x][y] != -1;
+    /*
+    如果是直线运动，则结果会是 D * 目标坐标点的地形权重，比如沙漠权重高点，平地权重就低点，即优先走平地
+    */
+    return weight * map[dest_x][dest_y]; 
 }
 
-vector<pair<int, int>> aStar::search(vector<vector<int>>& map, int start_x, int start_y, int end_x, int end_y)
+vector<pair<int, int>> aStar::search(const Map& map, int start_x, int start_y, int end_x, int end_y)
 {
-    this->row = map.size();
-    this->col = map[0].size();
-    if (_isValid(map, start_x, start_y) or _isValid(map, end_x, end_y))
+    if (!_isValid(map, start_x, start_y) or !_isValid(map, end_x, end_y)) // 起始/终点坐标异常
     {
         return {};
     }
-    auto startNode = make_shared<Node>(start_x, start_y);
-    startNode->setWeight(map[start_x][start_y]);
-    auto endNode = make_shared<Node>(end_x, end_y);
-    endNode->setWeight(map[end_x][end_y]);
-    priority_queue<NodePtr, vector<NodePtr>, cmp> myQue;
-    startNode->setG(0);
-    startNode->setH(_calH(startNode, endNode));
-    myQue.emplace(startNode);
-    vector<NodePtr> closeList;
-    vector<pair<int, int>> pathList;
-    while (!myQue.empty())
+    _maxX = map.size();
+    _maxY = map[0].size();
+    unordered_map<int, Node*> nodeMap;
+    Node* startNode = new Node(start_x, start_y);
+    nodeMap[startNode->x + _maxX * startNode->y] = startNode; // x + maxX * y 这种方式可以保证x和y计算出来的值是唯一的
+    startNode->g = 0;
+    startNode->h = _calH(startNode->x, startNode->y, end_x, end_y);
+    priority_queue<Node*, vector<Node*>, cmp> Que; // 使用 priority_queue 会有重复插入的问题，因此需要使用close字段进行判断，和dijkstra算法处理相似
+    Que.emplace(startNode);
+    vector<pair<int, int>> ret;
+    while (!Que.empty())
     {
-        auto currentNode = myQue.top();
-        myQue.pop();
-        if (_isEndNode(currentNode, endNode))
+        Node* parentNode = Que.top();
+        Que.pop();
+        if (parentNode->close) continue;
+        parentNode->close = true;
+        if (parentNode->x == end_x && parentNode->y == end_y)
         {
-            while (1)
+            while (parentNode->parent != nullptr)
             {
-                pathList.emplace_back(currentNode->getX(), currentNode->getY());
-                if (currentNode->getParent())
+                ret.emplace_back(parentNode->x, parentNode->y);
+                parentNode = parentNode->parent;
+            }
+            ret.emplace_back(parentNode->x, parentNode->y);
+            // 内存释放
+            for (auto iter = nodeMap.begin(); iter != nodeMap.end(); ++iter) delete iter->second;
+            return ret;
+        }
+        for (int i = 0; i < path.size(); ++i)
+        {
+            int new_x = path[i].first + parentNode->x;
+            int new_y = path[i].second + parentNode->y;
+            if (!_isValid(map, new_x, new_y)) continue;
+            int idx = new_x + _maxX * new_y;
+            if (nodeMap.find(idx) != nodeMap.end()) // 已经遍历过
+            {
+                Node* childNode = nodeMap[idx];
+                if (childNode->close) continue;
+                int new_g = _calG(map, new_x, new_y, i < 4 ? D : DD); // 直线 or 斜线，对于重复的节点，这里就会由于父节点位置的不同产生不同的值
+                int new_h = _calH(new_x, new_y, end_x, end_y);
+                if (new_g + new_h < childNode->g + childNode->h) // 有更小的权重，这里会造成重复插入，上面有过滤，因此无妨
+                {
+                    childNode->g = new_g;
+                    childNode->h = new_h;
+                    childNode->parent = parentNode;
+                    Que.emplace(childNode);
+                }
+            }
+            else
+            {
+                Node* childNode = new Node(new_x, new_y);
+                nodeMap[idx] = childNode;
+                int new_g = _calG(map, new_x, new_y, i < 4 ? D : DD); // 直线 or 斜线
+                int new_h = _calH(new_x, new_y, end_x, end_y);
+                childNode->g = new_g;
+                childNode->h = new_h;
+                childNode->parent = parentNode;
+                Que.emplace(childNode);
             }
         }
-        if (currentNode->isClose()) continue;
-        p->setClose();
-        _searchNear(currentNode, endNode, myQue, )
     }
-
+    return {}; // 没有找到路径
 }
-
-inline void aStar::_searchNear(const NodePtr& currentNode, const NodePtr& endNode)
-{
-    static vector<pair<int, int>> direct{
-        {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}
-    };
-
-    for (auto& p: direct)
-    {
-        
-    }
-}
-
-
 
 
 int main()
 {
-    auto startNode = make_shared<Node>(new Node(1,1));
-    auto endNode = make_shared<Node>(new Node(10, 10));
+    // vector<vector<int>> map{ // -1表示阻挡,其他数值表示当前点的权重值,值越大代表地形越"难走",优先级越低
+    //     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    //     {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    //     {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1},
+    //     {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
+    //     {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1},
+    //     {1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+    //     {1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1},
+    //     {1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1},
+    //     {1, 0, 0, 1, 0, 0, 0, 0 ,0, 1, 0 ,0, 1, 0, 0, 0, 0, 1, 0, 1},
+    //     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    // };
     vector<vector<int>> map{ // -1表示阻挡,其他数值表示当前点的权重值,值越大代表地形越"难走",优先级越低
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1},
-        {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
-        {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1},
-        {1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1},
-        {1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1},
-        {1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1},
-        {1, 0, 0, 1, 0, 0, 0, 0 ,0, 1, 0 ,0, 1, 0, 0, 0, 0, 1, 0, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+        {-1,    -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1},
+        {-1,    0,      0,      0,      0,      0,      1,      0,      -1,     -1},
+        {-1,    0,      0,      0,      0,      0,      1,      0,      -1,     -1},
+        {-1,    0,      -1,     -1,     -1,     -1,     -1,     -1,     0,      -1},
+        {-1,    0,      0,      0,      0,      0,      1,      0,      -1,     -1},
+        {-1,    0,      0,      0,      0,      0,      1,      1,      0,      -1},
+        {-1,    -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1}
     };
     aStar a;
-    auto path = a.search(map, 1, 1, 10, 10);
+    auto path = a.search(map, 1, 1, 5, 7);
+    for (auto & p: path)
+    {
+        cout << "(" << p.first << ", " << p.second << ")" << "=> ";
+    } 
+    cout << endl;
 }
