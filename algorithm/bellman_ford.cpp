bbf9e1165fad338aee7a8e544c6ae859ae7a773e
rxsi_xx_lt <rxsi_xx_lt>
2022-07-10 11:39:08 +0800

    bellman_ford算法，单源最短路径

--- algorithm/bellman_ford.cpp
+++ algorithm/bellman_ford.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <limits.h>
 using namespace std;
 
 /*
 单源最短路径算法
 相较于dijkstra算法，这个算法可以包含负权边和负权环
 在伪代码中返回的是true/false，即用来检测是否有环
-时间复杂度是O(V*E)，V是顶点数，E是边的数量
+时间复杂度是O(V*E)，V是顶点数，E是边的数量，这个是在当为领接表时才有的性质
 
 1. 创建源顶点 v 到图中所有顶点的距离的集合 distSet，为图中的所有顶点指定一个距离值，初始均为 Infinite，源顶点距离为 0；
 2. 计算最短路径，执行 V - 1 次遍历；
@@ -17,25 +18,48 @@ using namespace std;
 
 #define M INT_MAX/2
 
-pair<bool, vector<int>> bellmanFordFinder(int point, vector<vector<int>>& dist)
+vector<int> result;
+
+bool bellmanFordFinder(int point, vector<vector<int>>& dist) // 这里用的是邻接矩阵
 {
-    vector<int> result(dist.size(), M);
-    result[point] = 0;
-    for (int i = 1; i < dist.size(); ++i)
+    int n = dist.size();
+    result = vector<int>(n, M);
+    for (int i = 0; i < n; ++i) result[i] = dist[point][i]; // 初始距离
+    for (int i = 0; i < n; ++i)
     {
-        for (auto)
+        for (int j = 0; j < n; ++j)
+        {
+            if (dist[i][j] + result[i] < result[j])
+            {
+                result[j] = dist[i][j] + result[i];
+            }
+        }
     }
+    for (int i = 0 ; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            if (result[j] > dist[i][j] + result[i]) return false;
+        }
+    }
+    return true;
 };
 
 int main()
 {
     vector<vector<int>> dist {
-        {0, 5, M, 10},
-        {M, 0, 3, M},
+        {0, -5, M, 10},
+        {-3, 0, 3, M},
         {M, M, 0, 1},
         {M, M, M, 0}
     };
-    vector<int> ret = dijkstraFinder(0, dist);
-    for (auto& num: ret) cout << num << " ";
+    if (bellmanFordFinder(0, dist))
+    {
+        for (auto& num: result) cout << num << " ";    
+    }
+    else
+    {
+        cout << "have ring";
+    }
     cout << endl;
 } 
