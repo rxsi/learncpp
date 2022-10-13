ec46c848838130c4b3a82680af5558b8f4a1c6e7
rxsi_dy_pc <rxsi_dy_pc>
2022-10-13 10:08:22 +0800

    modify file_test

--- file_test.cpp
+++ file_test.cpp
@@ -115,6 +115,7 @@ int newSocketFlag = oldSocketFlag | O_NONBLOCK; // 新属性
 fcntl(listenfd, F_SETFL, newSocketFlag) // 设置属性
 
 C/C++平台的语句
+带有缓冲区，因此性能效率是高于linux原生语句的
 1. fopen：C/C++下的标准IO库函数，带输入/输出缓冲，在linux平台，该函数的底层实现就是open函数，因此这个函数具有可移植性
 与open的最大区别是该函数只能处理普通文件，且以流形式处理。而open可以处理普通文件和设备文件
 open函数返回的是文件句柄，而fopen返回的是文件指针，即指向FILE结构体的指针
@@ -246,12 +247,6 @@ int main()
     fwrite(d, 1, sizeof(a), fd);
     fwrite(e, 1, sizeof(a), fd);
     fwrite(f, 1, sizeof(a), fd);
-    // fwrite(a, sizeof(a), 1, fd);
-    // fwrite(b, sizeof(b), 1, fd);
-    // fwrite(c, sizeof(c), 1, fd);
-    // fwrite(d, sizeof(d), 1, fd);
-    // fwrite(e, sizeof(e), 1, fd);
-    // fwrite(f, sizeof(f), 1, fd);
     fclose(fd);
 }
 
@@ -263,31 +258,31 @@ int main()
 //     return st.st_size;
 // }
 
-//  /*
-// 1. 多线程读
-// */
-// void readFunc(int fd)
-// {
-//     char buf[10] = {0};
-//     int i = 5;
-//     while (i--)
-//     {
-//         ssize_t len = read(fd, buf, 10);
-//         std::cout << "threadID: " << std::this_thread::get_id() << ", data_len: " << len << ", data: " << buf << std::endl;
-//         std::cout << "tell: " << tellg(fd) << std::endl;
-//         std::this_thread::sleep_for(std::chrono::seconds(1));
-//     }
-// }
+ /*
+1. 多线程读
+*/
+void readFunc(FILE *stream)
+{
+    char buf[11];
+    int i = 5;
+    while (i--)
+    {
+        ssize_t len = fread(buf, 1, sizeof(buf), stream);
+        std::cout << "threadID: " << std::this_thread::get_id() << ", data_len: " << len << ", data: " << buf << std::endl;
+        std::cout << "tell: " << ftell(stream) << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
 
-// int main()
-// {
-//     int fd = open("/home/rxsi/hello_world.txt", O_RDONLY);
-//     std::thread t1(readFunc, std::ref(fd));
-//     std::thread t2(readFunc, std::ref(fd));
-//     t1.join();
-//     t2.join();
-//     close(fd);
-// }
+int main()
+{
+    FILE *stream = fopen("/home/rxsi/hello_world.txt", "r");
+    std::thread t1(readFunc, stream);
+    std::thread t2(readFunc, stream);
+    t1.join();
+    t2.join();
+    fclose(stream);
+}
 
 // /*
 // 2. 多线程write
