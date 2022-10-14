036f269aadc94391245bb62bae80af64e4b04aed
rxsi_dy_pc <rxsi_dy_pc>
2022-10-14 15:22:01 +0800

    modify

--- file_test.cpp
+++ file_test.cpp
@@ -571,6 +571,8 @@ int main()
     pid_t pid = fork();
     if (pid == 0) // 子进程
     {
+        std::this_thread::sleep_for(std::chrono::seconds(10));
         readFunc(stream);
     }
+    std::this_thread::sleep_for(std::chrono::seconds(20));
 }
