37167f3d88e04e1d5349cd7bb18c96e2efa2e71f
rxsi_dy_pc <rxsi_dy_pc>
2022-10-19 10:31:23 +0800

    使用不带缓存的

--- file_test.cpp
+++ file_test.cpp
@@ -768,10 +768,10 @@ void readFunc(int fd)
     char temp[1];
     size_t len = read(fd, temp, sizeof(temp));
     std::cout << "len: " << len << ", temp: " << temp << std::endl;
-    size_t len = read(fd, temp, sizeof(temp));
-    std::cout << "len: " << len << ", temp: " << temp << std::endl;
-    size_t len = read(fd, temp, sizeof(temp));
-    std::cout << "len: " << len << ", temp: " << temp << std::endl;
+    size_t len2 = read(fd, temp, sizeof(temp));
+    std::cout << "len2: " << len2 << ", temp: " << temp << std::endl;
+    size_t len3 = read(fd, temp, sizeof(temp));
+    std::cout << "len3: " << len3 << ", temp: " << temp << std::endl;
     // while (i--)
     // {
     //     std::this_thread::sleep_for(std::chrono::seconds(1));
