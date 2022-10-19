647574e1d84534799a4bf681c33cf7b4fa18882d
rxsi_dy_pc <rxsi_dy_pc>
2022-10-19 10:28:24 +0800

    使用不带缓存的

--- file_test.cpp
+++ file_test.cpp
@@ -770,9 +770,10 @@ void readFunc(int fd)
     {
         std::this_thread::sleep_for(std::chrono::seconds(1));
         size_t len = read(fd, temp, sizeof(temp));
-        std::cout << temp << std::endl;
+        std::string new_temp(temp);
+        std::cout << new_temp << std::endl;
         strcpy(buf+step, temp);
-        std::cout << "buf: " << buf << ", size: " << sizeof(buf) << ", buf+step: " << &(buf+step) << std::endl;
+        std::cout << "buf: " << buf << ", size: " << sizeof(buf) << std::endl;
         step += 1;
     }
     std::string s(buf);
